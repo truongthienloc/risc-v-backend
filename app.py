d3a1403763b0fb5587a0c09f6d3b9d4f916f2e2d
unknown <truongthienloc2@gmail.com>
2023-09-30 10:51:22 +0700

    Develop Intergate Processing core into API

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request, json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
     return "Welcome to RISC-V Assembler"
+
+@app.route('/check-health')
+def check_health():
+    return "The server is running"
+
+import assembler
+import core
+import diassembler
 
 if __name__ == "__main__":
     app.run()
